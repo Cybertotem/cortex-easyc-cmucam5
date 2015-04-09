c65fc1983a191cc31644e97a77567903f7359106
Mike Pennell <mike@cybertotem.com>
2015-04-09 18:05:23 -0500

    Ran auto-format on file

--- PixyLib.h
+++ PixyLib.h
@@ -23,8 +23,8 @@
 #define PIXY_SYNC_BYTE_DATA         0x5b
 
 // Some easyC definitions
-#define DEFAULT_PIXY_PORT		2
-#define DEFAULT_PIXY_BAUD_RATE	19200
+#define DEFAULT_PIXY_PORT						2
+#define DEFAULT_PIXY_BAUD_RATE			19200
 
 // the routines
 //void PixyInit();
@@ -37,18 +37,18 @@ uint16_t getBlocks(uint16_t maxBlocks);
 // data types
 typedef enum 
 {
-    NORMAL_BLOCK,
-    CC_BLOCK // color code block
+		NORMAL_BLOCK,
+		CC_BLOCK // color code block
 } BlockType;
 
 typedef struct  
 {
-  uint16_t signature; 
-  uint16_t x;
-  uint16_t y;
-  uint16_t width;
-  uint16_t height;
-  uint16_t angle; // angle is only available for color coded blocks
+	uint16_t signature; 
+	uint16_t x;
+	uint16_t y;
+	uint16_t width;
+	uint16_t height;
+	uint16_t angle; // angle is only available for color coded blocks
 } Block;
 
 // communication routines
@@ -61,14 +61,14 @@ static uint8_t ReadSerialwBlock(void);
 
 uint16_t getWord(void)
 {
-  // this routine assumes little endian 
-  uint16_t w; 
-  uint8_t c;
-  c = ReadSerialwBlock();
-  w = ReadSerialwBlock();
-  w <<= 8;
-  w |= c; 
-  return w;
+	// this routine assumes little endian 
+	uint16_t w; 
+	uint8_t c;
+	c = ReadSerialwBlock();
+	w = ReadSerialwBlock();
+	w <<= 8;
+	w |= c; 
+	return w;
 }
 
 uint8_t ReadSerialwBlock(void)
@@ -87,11 +87,11 @@ uint8_t ReadSerialwBlock(void)
 /*
 int send(uint8_t *data, int len)
 {
-  int i;
-  for (i=0; i<len; i++)
-    sendByte(data[i]);
+	int i;
+	for (i=0; i<len; i++)
+		sendByte(data[i]);
 
-  return len;
+	return len;
 }
 */
 
@@ -101,134 +101,134 @@ static Block *g_blocks;
 
 void PixyInit()
 {
-  g_blocks = (Block *)malloc(sizeof(Block)*PIXY_ARRAYSIZE);
+	g_blocks = (Block *)malloc(sizeof(Block)*PIXY_ARRAYSIZE);
 }
 
 int PixyGetStart(void)
 {
-  uint16_t w, lastw;
+	uint16_t w, lastw;
 
-  lastw = 0xffff;
+	lastw = 0xffff;
 
-  while(1)
-  {
-    w = getWord();
-    if (w==0 && lastw==0)
-      return 0; // no start code  
-    else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD)
-    {
-      g_blockType = NORMAL_BLOCK;
-      return 1; // code found!
-    }
-    else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD)
-    {
-      g_blockType = CC_BLOCK; // found color code block
-      return 1;
-    }    
-    else if (w==PIXY_START_WORDX) 
-    ReadSerialwBlock(); // we're out of sync! (backwards)
-    lastw = w; 
-  }
+	while(1)
+	{
+		w = getWord();
+		if (w==0 && lastw==0)
+			return 0; // no start code  
+		else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD)
+		{
+			g_blockType = NORMAL_BLOCK;
+			return 1; // code found!
+		}
+		else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD)
+		{
+			g_blockType = CC_BLOCK; // found color code block
+			return 1;
+		}    
+		else if (w==PIXY_START_WORDX) 
+		ReadSerialwBlock(); // we're out of sync! (backwards)
+		lastw = w; 
+	}
 }
 
 uint16_t getBlocks(uint16_t maxBlocks)
 {
-  uint8_t i;
-  uint16_t w, blockCount, checksum, sum;
-  Block *block;
+	uint8_t i;
+	uint16_t w, blockCount, checksum, sum;
+	Block *block;
 
-  if (!g_skipStart)
-  {
-    if (PixyGetStart()==0)
-      return 0;
-  }
-  else
-    g_skipStart = 0;
+	if (!g_skipStart)
+	{
+		if (PixyGetStart()==0)
+			return 0;
+	}
+	else
+		g_skipStart = 0;
 
-  for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_ARRAYSIZE;)
-  {
-    checksum = getWord();
-    if (checksum==PIXY_START_WORD) // we've reached the beginning of the next frame
-    {
-      g_skipStart = 1;
-      g_blockType = NORMAL_BLOCK;
-      return blockCount;
-    }
-    else if (checksum==PIXY_START_WORD_CC)
-    {
-      g_skipStart = 1;
-      g_blockType = CC_BLOCK;
-      return blockCount;
-    }
-    else if (checksum==0)
-      return blockCount;
+	for(blockCount=0; blockCount<maxBlocks && blockCount<PIXY_ARRAYSIZE;)
+	{
+		checksum = getWord();
+		if (checksum==PIXY_START_WORD) // we've reached the beginning of the next frame
+		{
+			g_skipStart = 1;
+			g_blockType = NORMAL_BLOCK;
+			return blockCount;
+		}
+		else if (checksum==PIXY_START_WORD_CC)
+		{
+			g_skipStart = 1;
+			g_blockType = CC_BLOCK;
+			return blockCount;
+		}
+		else if (checksum==0)
+			return blockCount;
 
-    block = g_blocks + blockCount;
+		block = g_blocks + blockCount;
 
-    for (i=0, sum=0; i<sizeof(Block)/sizeof(uint16_t); i++)
-    {
-      if (g_blockType==NORMAL_BLOCK && i>=5) // no angle for normal block
-      {
-        block->angle = 0;
-        break;
-      }
-      w = getWord();
-      sum += w;
-      *((uint16_t *)block + i) = w;
-    }
+		for (i=0, sum=0; i<sizeof(Block)/sizeof(uint16_t); i++)
+		{
+			if (g_blockType==NORMAL_BLOCK && i>=5) // no angle for normal block
+			{
+				block->angle = 0;
+				break;
+			}
+			w = getWord();
+			sum += w;
+			*((uint16_t *)block + i) = w;
+		}
 
-    // check checksum
-    if (checksum==sum)
-      blockCount++;
-    else
-      //printf("checksum error!\n");
-      PrintToScreen("checksum error!\n");
+		// check checksum
+		if (checksum==sum)
+			blockCount++;
+		else
+			//printf("checksum error!\n");
+			PrintToScreen("checksum error!\n");
 
-    w = getWord();
-    if (w==PIXY_START_WORD)
-      g_blockType = NORMAL_BLOCK;
-    else if (w==PIXY_START_WORD_CC)
-      g_blockType = CC_BLOCK;
-    else
-      return blockCount;
-  }
+		w = getWord();
+		if (w==PIXY_START_WORD)
+			g_blockType = NORMAL_BLOCK;
+		else if (w==PIXY_START_WORD_CC)
+			g_blockType = CC_BLOCK;
+		else
+			return blockCount;
+	}
 }
 
 /*
 int setServos(uint16_t s0, uint16_t s1)
 {
-  uint8_t outBuf[6];
+	uint8_t outBuf[6];
 
-  outBuf[0] = 0x00;
-  outBuf[1] = PIXY_SERVO_SYNC; 
-  *(uint16_t *)(outBuf + 2) = s0;
-  *(uint16_t *)(outBuf + 4) = s1;
+	outBuf[0] = 0x00;
+	outBuf[1] = PIXY_SERVO_SYNC; 
+	*(uint16_t *)(outBuf + 2) = s0;
+	*(uint16_t *)(outBuf + 4) = s1;
 
-  return send(outBuf, 6);
+	return send(outBuf, 6);
 }
 
 int setBrightness(uint8_t brightness)
 {
-  uint8_t outBuf[3];
+	uint8_t outBuf[3];
 
-  outBuf[0] = 0x00;
-  outBuf[1] = PIXY_CAM_BRIGHTNESS_SYNC; 
-  outBuf[2] = brightness;
+	outBuf[0] = 0x00;
+	outBuf[1] = PIXY_CAM_BRIGHTNESS_SYNC; 
+	outBuf[2] = brightness;
 
-  return send(outBuf, 3);
+	return send(outBuf, 3);
 }
 
 int setLED(uint8_t r, uint8_t g, uint8_t b)
 {
-  uint8_t outBuf[5];
+	uint8_t outBuf[5];
 
-  outBuf[0] = 0x00;
-  outBuf[1] = PIXY_LED_SYNC; 
-  outBuf[2] = r;
-  outBuf[3] = g;
-  outBuf[4] = b;
+	outBuf[0] = 0x00;
+	outBuf[1] = PIXY_LED_SYNC; 
+	outBuf[2] = r;
+	outBuf[3] = g;
+	outBuf[4] = b;
 
-  return send(outBuf, 5);
+	return send(outBuf, 5);
 }
 */
 
